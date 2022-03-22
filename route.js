91bf76eac61524a9323d716b5b370af64a0e20b1
fatiudeen <shehufatiudeen@gmail.com>
2022-03-22 16:58:57 +0100

    init

---
+++ route.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { db, bucket } from './db.js';
+import UUID from 'uuid-v4';
+import upload from './upload.js';
+import path from 'path';
+import fs from 'fs';
+
+const router = express.Router();
+let uuid = UUID();
+
+router.get('/post', async (req, res) => {
+  try {
+    let post = [];
+    let doc = await db.collection('posts').get();
+    doc.forEach((val) => {
+      post.push(val.data());
+    });
+    res.status(201).send(post);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// caption location time
+router.post('/post', upload, async (req, res) => {
+  try {
+    // Uploads a local file to the bucket
+    let date = Date.now();
+    let id = uuid;
+    let uploaded = await bucket.upload(req.file.path, {
+      uploadType: 'media',
+      metadata: {
+        metadata: {
+          firebaseStorageDownloadTokens: id,
+          contentType: req.file.mimetype,
+        },
+      },
+    });
+    let post = await db.collection('posts').doc(id).set({
+      id,
+      caption: req.body.caption,
+      location: req.body.location,
+      date,
+      imageUrl: 'post',
+    });
+    fs.unlink('./' + req.file.path, (err) => {
+      throw new Error(err);
+    });
+
+    res.status(201).send({ success: post });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
+
+export default router;
---
+++ upload.js
@@ -0,0 +1,23 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './tmp'); // create folder
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix);
+  },
+});
+
+const fileFilter = (req, res, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(err, false);
+  }
+};
+
+export default multer({ storage: storage }, { fileFilter: fileFilter }).single(
+  'image'
+);
